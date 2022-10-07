779d9117e29760a48559288c9e0eaaacae70d08e
mrmissx <hi@mrmiss.my.id>
2022-10-07 17:44:45 +0700

    fix(about): edit about page API endpoint and style

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <div className={styles.content}>
                 <h1>StashBin | simple pastebin for your text file.</h1>
                 <p>
-                    Paste, save and share your file easily to everyone in the world
+                    Paste, save and share your file easily to everyone in the world.
                     <br />
                     Stashbin is an open source pastebin service. You can find the source code on{" "}
                     <a href="https://github.com/mrmissx/stashbin">Github.</a>
@@ -17,19 +17,18 @@ export default function Home() {
                 <h2>Document Lifetime</h2>
                 <p>
                     StashBin have no explicit document expiration time. However old documents may be
-                    removed at any time without notice
+                    removed at any time without notice.
                 </p>
                 <h2>API endpoint</h2>
                 <p>
                     You can use the API to create, read, update, and delete documents. You can send
-                    a http POST with a JSON body containing field named &apos;content&apos;. API
-                    only accepts &apos;Content-Type: application/json&apos;. Other than that, it
-                    will response a Bad Request. Here an example using curl.
+                    a http POST with a JSON body containing field named &apos;content&apos;. Other
+                    than that, it will response a Bad Request. Here an example using curl.
                 </p>
                 <pre>
                     <code>
                         {`curl -X POST \\
-  'https://stashbin.xyz//api/document' \\
+  'https://stashbin.xyz/api/document' \\
   --header 'Content-Type: application/json' \\
   --data-raw '{
   "content": "Hi Mom"
@@ -37,8 +36,11 @@ export default function Home() {
                     </code>
                 </pre>
                 <p>
-                    And you can get your document with the key received from POST on{" "}
-                    <pre>https://stashbin.xyz/api/document?key=yourKey</pre>
+                    And you can GET your document with the key received from POST on{" "}
+                    <pre>
+                        <code>{`curl -X GET \\
+  'https://stashbin.xyz/api/document?key=examplekey'`}</code>
+                    </pre>
                 </p>
             </div>
             <Footer />
