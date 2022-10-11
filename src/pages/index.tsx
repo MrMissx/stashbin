e9d715a1361a745862fc0fe1a15bba3fe0f35402
Gaung Ramadhan <hi@mrmiss.my.id>
2022-10-11 14:33:02 +0700

    fix(index): add check to make sure only one save request sent (#7)
    resolve #1
    
    This also move shortcut handler directly to the HTML element instead
    adding manually on useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next"
 import { useRouter } from "next/router"
-import { useState, useEffect, useRef, useCallback } from "react"
+import { useState, useRef } from "react"
 
 import Header from "../components/Header"
 import Footer from "../components/Footer"
@@ -12,6 +12,7 @@ const Home: NextPage = () => {
     const [saveDisable, setSaveDisable] = useState(true)
 
     const saveContent = async () => {
+        setSaveDisable(true)
         const response = await fetch("/api/document", {
             method: "POST",
             headers: {
@@ -27,24 +28,9 @@ const Home: NextPage = () => {
             router.push(`/${result.data.key}`)
         } else {
             alert(`Failed to save document: ${result.err}`)
+            setSaveDisable(false)
         }
     }
-    const save = useCallback(saveContent, [router])
-
-    useEffect(() => {
-        const ref = contentRef.current
-        ref?.addEventListener("keydown", (event) => {
-            // handle ctrl + s
-            if (event.ctrlKey && event.key === "s") {
-                event.preventDefault()
-                save()
-                event.stopImmediatePropagation() // prevent triggering twice
-            }
-        })
-        return () => {
-            ref?.removeEventListener("keydown", () => {})
-        }
-    }, [save])
 
     return (
         <main className={styles.main}>
@@ -56,6 +42,26 @@ const Home: NextPage = () => {
                     placeholder="Write or paste, save and share the link..."
                     onChange={(event) => {
                         setSaveDisable(event.target.value.length === 0)
+                    }}
+                    onKeyDown={(event) => {
+                        if (event.key === "s" && event.ctrlKey) {
+                            // handle ctrl + s
+                            event.preventDefault()
+                            if (!saveDisable) {
+                                saveContent()
+                            }
+                        }
+                        if (event.key === "Tab") {
+                            // handle tab indentation
+                            event.preventDefault()
+                            const ref = contentRef.current
+                            if (!ref) return
+                            const start = ref?.selectionStart
+                            const end = ref?.selectionEnd
+                            ref.value =
+                                ref.value.substring(0, start) + "\t" + ref.value.substring(end)
+                            ref.selectionStart = ref.selectionEnd = start + 1
+                        }
                     }}
                 />
             </div>
