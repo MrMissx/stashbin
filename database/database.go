d71968c970f55f944be340a7469b6d84b9835037
mrmissx <hi@mrmiss.dev>
2024-07-31 23:30:54 +0700

    feat: run db migration on startup

--- database/database.go
+++ database/database.go
@@ -1,14 +1,39 @@
 package database
 
 import (
+	"database/sql"
 	"time"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/mrmissx/stashbin/utils"
 )
 
 var session *sqlx.DB
+
+func runMigrations(instance *sql.DB) {
+	utils.Logger.Info("Running migrations...")
+	driver, err := postgres.WithInstance(instance, &postgres.Config{})
+	if err != nil {
+		utils.Logger.Fatal("Could not create migrate driver: ", err)
+	}
+	m, err := migrate.NewWithDatabaseInstance("file://database/migrations", "postgres", driver)
+	if err != nil {
+		utils.Logger.Fatal("Failed to create migration instance: ", err)
+	}
+	if err := m.Up(); err != nil {
+		if err == migrate.ErrNoChange {
+			utils.Logger.Info("Database schema is up to date")
+			return
+		} else {
+			utils.Logger.Fatal("Failed to apply migrations")
+		}
+	}
+	utils.Logger.Info("Migrations completed")
+}
 
 func ConnectDb() {
 	dbUri := utils.GetEnv("DB_URI", "")
@@ -23,9 +48,11 @@ func ConnectDb() {
 	db.SetMaxOpenConns(10)
 	db.SetConnMaxIdleTime(10 * time.Minute)
 	db.SetConnMaxLifetime(1 * time.Hour)
+	utils.Logger.Info("Successfuly connected database")
+
+	runMigrations(db.DB)
 
 	session = db
-	utils.Logger.Info("Successfuly connected database")
 }
 
 func GetDatabase() *sqlx.DB {
--- go.mod
+++ go.mod
@@ -3,26 +3,30 @@ module github.com/mrmissx/stashbin
 go 1.21.6
 
 require (
-	github.com/a-h/templ v0.2.543
-	github.com/labstack/echo/v4 v4.11.4
+	github.com/a-h/templ v0.2.747
+	github.com/labstack/echo/v4 v4.12.0
 )
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/jmoiron/sqlx v1.3.5
+	github.com/golang-migrate/migrate/v4 v4.17.1
+	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/lib/pq v1.10.9
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-sqlite3 v1.14.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 )
